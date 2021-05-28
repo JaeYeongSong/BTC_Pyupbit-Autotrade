66d5280225c7d68e2e5da7003d6b79d422ad9536
Xsop-sop <70881925+Xsop-sop@users.noreply.github.com>
2021-05-28 19:28:55 +0900

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,10 @@
+import requests
+
+def post_message(token, channel, text):
+    """슬랙 메시지 전송"""
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    
+post_message(myToken,"#stock", "hello")
